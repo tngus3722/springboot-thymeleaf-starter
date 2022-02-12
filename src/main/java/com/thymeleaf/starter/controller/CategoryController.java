203be1ffeb037f8c7c70ad1f7b40b19b91f37dbd
ethan.jung <ethan.jung@musinsa.com>
2022-02-12 23:09:07 +0900

    add category 1,2 depth

--- src/main/java/com/thymeleaf/starter/controller/CategoryController.java
+++ src/main/java/com/thymeleaf/starter/controller/CategoryController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,17 +16,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class CategoryController {
 
     private final CategoryService categoryService;
 
     @GetMapping("/category")
-    public ResponseEntity<List<CategoryResponse>> getCategory() {
-        return new ResponseEntity<>(categoryService.getCategory(), HttpStatus.OK);
+    public ModelAndView getCategory() {
+        ModelAndView modelAndView = new ModelAndView("/category/index");
+        modelAndView.addObject("categories", categoryService.getCategory());
+        return modelAndView;
     }
 
     @GetMapping("/category/{category_id}")
